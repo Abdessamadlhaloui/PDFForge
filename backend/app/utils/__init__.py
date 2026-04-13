de8624b17b554c2a808004a8841647fc54cff607
Abdessamad Lahlaoui <abdessamadlahlaoui0@gmail.com>
2026-04-13 21:00:02 +0000

    feat(backend/utils): initialize utilities package

---
+++ backend/app/utils/__init__.py
@@ -0,0 +1,63 @@
+from app.utils.file_handler import (
+
+    cleanup_expired_files,
+
+    delete_file,
+
+    find_file,
+
+    generate_file_id,
+
+    generate_task_id,
+
+    get_download_url,
+
+    get_expiry_time,
+
+    get_file_path,
+
+    get_file_size,
+
+    get_mime_type,
+
+    save_bytes,
+
+    save_bytes_sync,
+
+    save_upload,
+
+    validate_pdf,
+
+)
+
+__all__ = [
+
+    "cleanup_expired_files",
+
+    "delete_file",
+
+    "find_file",
+
+    "generate_file_id",
+
+    "generate_task_id",
+
+    "get_download_url",
+
+    "get_expiry_time",
+
+    "get_file_path",
+
+    "get_file_size",
+
+    "get_mime_type",
+
+    "save_bytes",
+
+    "save_bytes_sync",
+
+    "save_upload",
+
+    "validate_pdf",
+
+]
